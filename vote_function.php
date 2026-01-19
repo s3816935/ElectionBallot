55bd1ab76d9ba5f8d3108f3d3431fe9fc2b160d7
Jacky Zhu <53916990+s3816935@users.noreply.github.com>
2026-01-20 10:34:06 +1100

    Update vote_function.php

--- vote_function.php
+++ vote_function.php
@@ -1,6 +1,6 @@
 <?php 
 $username = 'S3816935';
-$password = 'orABGD@2023!';
+$password = '2023';
 $servername = 'talsprddb01.int.its.rmit.edu.au';
 $servicename = 'CSAMPR1.ITS.RMIT.EDU.AU';
 $connection = $servername."/".$servicename;
